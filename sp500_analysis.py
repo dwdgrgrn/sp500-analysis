Fix the future close value calculation mistake, replace '+' with '*'

sp500_analysis.py
@@ -17,12 +17,14 @@ std = np.sqrt(var)
 
 current_close = close_values[-1]
 days_into_future = 365
-num_of_simulations = 1000
+num_of_simulations = 10000
 
 future_close_values = np.empty((num_of_simulations, days_into_future))
 
 for i in range(num_of_simulations):
     latest_close = current_close
     for j in range(days_into_future):
-        future_close_values[i, j] = latest_close + np.exp(np.random.normal(mean, std))
+        future_close_values[i, j] = latest_close * np.exp(np.random.normal(mean, std))
         latest_close = future_close_values[i, j]
+
+print(np.sort(future_close_values[-1])[0])