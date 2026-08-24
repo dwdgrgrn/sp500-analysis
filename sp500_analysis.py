bf6b2140aa97dfea5019328e1abff35fd55660ec
Dawid <dgrigorian2006@gmail.com>
2026-08-24 15:53:44 +0100

    Simulate future close values to 365 days into the future

--- sp500_analysis.py
+++ sp500_analysis.py
@@ -10,5 +10,19 @@ log_returns = []    # Array of log returns at the end of each day
 for i in range(close_values.size - 1):
     log_returns.append(np.log(close_values[i+1] / close_values[i]))
 
-print(np.mean(log_returns))
-print(np.var(log_returns))
+mean = np.mean(log_returns)
+var = np.var(log_returns)
+std = np.sqrt(var)
+
+
+current_close = close_values[-1]
+days_into_future = 365
+num_of_simulations = 1000
+
+future_close_values = np.empty((num_of_simulations, days_into_future))
+
+for i in range(num_of_simulations):
+    latest_close = current_close
+    for j in range(days_into_future):
+        future_close_values[i, j] = latest_close + np.exp(np.random.normal(mean, std))
+        latest_close = future_close_values[i, j]
