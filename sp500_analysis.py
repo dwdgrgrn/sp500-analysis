5d55b97c493698b3c962e6906361d2a9e680a325
Dawid <dgrigorian2006@gmail.com>
2026-08-24 15:36:38 +0100

    Calculate mean and variance for log returns over the last five years

--- sp500_analysis.py
+++ sp500_analysis.py
@@ -1,5 +1,14 @@
 import yfinance as yf
+import numpy as np
 
-data = yf.Ticker("^GSPC").history("ytd")
+data = yf.Ticker("^GSPC").history("5y")
 
-print(data.head())
+close_values = data["Close"].values
+
+log_returns = []    # Array of log returns at the end of each day
+
+for i in range(close_values.size - 1):
+    log_returns.append(np.log(close_values[i+1] / close_values[i]))
+
+print(np.mean(log_returns))
+print(np.var(log_returns))
