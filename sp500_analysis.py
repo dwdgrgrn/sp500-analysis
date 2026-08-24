a42c3be2927a3b0741186e62717566574c602c8a
Dawid <dgrigorian2006@gmail.com>
2026-08-24 16:38:33 +0100

    Calculate loss (or gain) at a given confidence interval and time period

--- sp500_analysis.py
+++ sp500_analysis.py
@@ -5,7 +5,7 @@ data = yf.Ticker("^GSPC").history("5y")
 
 close_values = data["Close"].values
 
-log_returns = []    # Array of log returns at the end of each day
+log_returns = []    # Array of log returns at close of each day
 
 for i in range(close_values.size - 1):
     log_returns.append(np.log(close_values[i+1] / close_values[i]))
@@ -16,7 +16,7 @@ std = np.sqrt(var)
 
 
 current_close = close_values[-1]
-days_into_future = 365
+days_into_future = 365*5
 num_of_simulations = 10000
 
 future_close_values = np.empty((num_of_simulations, days_into_future))
@@ -27,4 +27,23 @@ for i in range(num_of_simulations):
         future_close_values[i, j] = latest_close * np.exp(np.random.normal(mean, std))
         latest_close = future_close_values[i, j]
 
-print(np.sort(future_close_values[-1])[0])
+final_close_values = np.sort(future_close_values[:, -1])
+
+confidence_level = 0.95
+percentile = 1 - confidence_level
+
+percentile_index = int(percentile * num_of_simulations)
+
+percentile_price = final_close_values[percentile_index]
+percentage_difference = ((final_close_values[percentile_index] / current_close) - 1) * 100
+
+print("\n")
+print(f"Confidence Level:      {confidence_level * 100}%")
+print(f"Simulations:           {num_of_simulations}")
+print(f"Days into Future:      {days_into_future}\n")
+
+print(f"Current Close:         {current_close}")
+print(f"Simulation Close:      {percentile_price}\n")
+
+
+print(f"Percentage Difference: {percentage_difference:.2f}%\n")
